27b936988d68c0dd0d8efcee50557d536ef80a15
Julia <113190885+xisanex@users.noreply.github.com>
2023-01-16 13:48:57 +0100

    implementing auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutmeContactComponent } from './aboutme-contact/aboutme-contact.component';
 import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { ImageComponent } from './image/image.component';
 import { RatingComponent } from './rating/rating.component';
@@ -9,7 +10,11 @@ import { RatingComponent } from './rating/rating.component';
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'rating', component: RatingComponent },
-  { path: 'aboutMe|contact', component: AboutmeContactComponent },
+  {
+    path: 'aboutMe|contact',
+    component: AboutmeContactComponent,
+    canActivate: [AuthGuard],
+  },
   { path: 'addimage', component: ImageComponent },
   { path: 'auth', component: AuthComponent },
 ];
---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { map, Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    return this.authService.user.pipe(
+      // take(1),
+      map(user => {
+        const isAuth = !!user;
+        if (isAuth) {
+          return true;
+        }
+        this.router.navigate(['auth']);
+        return false;
+      })
+    );
+  }
+}
